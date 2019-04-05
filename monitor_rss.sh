fb3ae899d0d4bbc2cef8ed9404693181ce54bcc9
Burr Sutter <burrsutter@gmail.com>
2019-04-05 19:21:12 -0400

    tweaks

--- monitor_rss.sh
+++ monitor_rss.sh
@@ -5,3 +5,10 @@ while true; do ps ax -o pid,rss,command | grep "[g]onow"; sleep .3; done
 while true; do ps ax -o pid,rss,command | grep "[b]ooted"; sleep .3; done
 
 while true; do ps ax -o pid,rss,command | grep "[q]uarked"; sleep .3; done
+
+OR
+
+alias ps-rss='ps ax -o pid,rss,command | numfmt --header --from-unit=1024 --to=iec --field 2|grep -v grep'
+
+ps-rss | grep "whatever"
+
