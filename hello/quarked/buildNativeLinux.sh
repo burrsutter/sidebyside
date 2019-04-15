18e3524c7980932e5e230835dd1258399d8c78d1
Burr Sutter <burrsutter@gmail.com>
2019-04-15 08:27:34 -0400

    new graalvm

--- hello/quarked/buildNativeLinux.sh
+++ hello/quarked/buildNativeLinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export GRAALVM_HOME=~/tools/graalvm-ce-1.0.0-rc12/Contents/Home/
+export GRAALVM_HOME=~/tools/graalvm-ce-1.0.0-rc15/Contents/Home/
 
 # Mac Native
 # mvn package -Pnative
