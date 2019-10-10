fd73f435c43f02d93421778bd3983bbb72e07c87
Burr Sutter <burrsutter@gmail.com>
2019-10-10 19:05:18 -0400

    adding minikube pollers

---
+++ hello/python/knpoller_minikube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+INGRESSGATEWAY=istio-ingressgateway
+IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY -n istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
+HOST_URL=$(kubectl get routes.serving.knative.dev pythoned -o jsonpath='{.status.url}')
+STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
+
+while true
+do curl -H "Host: ${STRIPPED}" $IP_ADDRESS
+sleep .3
+done
