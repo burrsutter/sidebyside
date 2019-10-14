d84f98bd49dc02aabbe28559b48efd83e65d27a0
Burr Sutter <burrsutter@gmail.com>
2019-10-14 14:54:30 -0400

    adding burst scripts

--- hello/booted/knburst_minikube.sh
+++ hello/booted/knburst_minikube.sh
@@ -2,6 +2,7 @@
 
 INGRESSGATEWAY=istio-ingressgateway
 IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
-HOST_URL=$(kubectl get routes.serving.knative.dev booted  -o jsonpath='{.status.domain}')
+HOST_URL=$(kubectl get routes.serving.knative.dev booted  -o jsonpath='{.status.url}')
+STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
 
-siege -r 1 -c 20 -d 2 -v -H "Host: ${HOST_URL}" ${IP_ADDRESS}
+siege -r 1 -c 30 -d 2 -v -H "Host: ${STRIPPED}" ${IP_ADDRESS}
--- hello/go/knburst_minikube.sh
+++ hello/go/knburst_minikube.sh
@@ -2,7 +2,7 @@
 
 INGRESSGATEWAY=istio-ingressgateway
 IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
-HOST_URL=$(kubectl get routes.serving.knative.dev noded  -o jsonpath='{.status.url}')
+HOST_URL=$(kubectl get routes.serving.knative.dev gonow  -o jsonpath='{.status.url}')
 STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
 
 siege -r 1 -c 30 -d 2 -v -H "Host: ${STRIPPED}" ${IP_ADDRESS}
