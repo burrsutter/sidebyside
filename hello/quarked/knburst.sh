78cfe25e1277a8c0a4388d46f8767b41a11b17de
Burr Sutter <burrsutter@gmail.com>
2019-10-14 13:47:48 -0400

    trying to get working poller scripts

--- hello/quarked/knburst.sh
+++ hello/quarked/knburst.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 INGRESSGATEWAY=istio-ingressgateway
-IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
-HOST_URL=$(kubectl get routes.serving.knative.dev quarked  -o jsonpath='{.status.domain}')
+IP_ADDRESS="$(minikube ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
+HOST_URL=$(kubectl get routes.serving.knative.dev quarked  -o jsonpath='{.status.url}')
+STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
 
-siege -r 1 -c 20 -d 2 -v -H "Host: ${HOST_URL}" ${IP_ADDRESS}
+siege -r 1 -c 20 -d 2 -v -H "Host: ${STRIPPED}" ${IP_ADDRESS}
--- hello/quarked/knpoller.sh
+++ hello/quarked/knpoller.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 INGRESSGATEWAY=istio-ingressgateway
-IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
+IP_ADDRESS="$(minikube --profile knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
 HOST_URL=$(kubectl get routes.serving.knative.dev quarked -o jsonpath='{.status.domain}')
+STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
 
 while true
-do curl -H "Host: ${HOST_URL}" $IP_ADDRESS
+do curl -H "Host: ${STRIPPED}" $IP_ADDRESS
 sleep .3
 done
